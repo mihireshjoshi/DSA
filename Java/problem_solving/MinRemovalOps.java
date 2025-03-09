52958b4590e3494d8a2de032555a75007562f0d7
mihireshjoshi <mihireshsjoshi@gmail.com>
2025-03-09 20:53:33 +0530

    Minimum Removal Operation solved in Java/problem_solving

---
+++ Java/problem_solving/MinRemovalOps.java
@@ -0,0 +1,62 @@
+package Java.problem_solving;
+
+
+import java.util.*;
+
+public class MinRemovalOps {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] arr = {2, 4, 6, 9};
+        System.out.println(sol.minOperation(arr, 1));
+    }
+}
+
+class Solution {
+    public static int minOperation(int[] arr, int k) {
+        int steps = 0;
+        Map<Integer, List<Integer>> diff = new TreeMap<>();
+        int sum = 0;
+
+
+        for (int num : arr) {
+            sum += num;
+        }
+        sum = sum / arr.length;
+
+        for (int i = 0; i < arr.length; i++) {
+            int diffValue = Math.abs(sum - arr[i]);
+            diff.putIfAbsent(diffValue, new ArrayList<>());
+            diff.get(diffValue).add(i);
+        }
+
+        while (k > 0 && !diff.isEmpty()) {
+            int key = ((TreeMap<Integer, List<Integer>>) diff).lastKey();
+            List<Integer> indices = diff.get(key);
+
+            while (!indices.isEmpty() && k > 0) {
+                indices.remove(indices.size() - 1);
+                k--;
+            }
+
+            if (indices.isEmpty()) {
+                diff.remove(key);
+            }
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (List<Integer> indexList : diff.values()) {
+            pq.addAll(indexList);
+        }
+
+        while(!pq.isEmpty()){
+            steps++;
+            int div = pq.peek()/2;
+            pq.removeIf(num->num>=div);
+        }
+
+        return steps;
+
+    }
+}
